3eaeb9afe6df0f6f278268646e2a5fe1cefd3f29
João Galrito <joao.galrito@hotmail.com>
2019-08-25 05:57:44 +0100

    simplified

--- utils.js
+++ utils.js
@@ -6,7 +6,7 @@ exports.readSample = (view, i, bytesPerSample) => {
 			sample |= view.getUint8(i+j) << (8 * j)
 
 		//sign extension for 32 bit
-		return sample & 0x800000 ? sample | 0xff000000 : sample
+		return sample << 8 >> 8
 	}
 
 	return view.getInt16(i, true)
